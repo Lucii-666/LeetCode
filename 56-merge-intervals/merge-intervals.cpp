1a7364591987cd1c81ed7c53e05ae86909ff12ba
Ashutosh Kumar Singh <144926351+Lucii-666@users.noreply.github.com>
2025-10-06 17:53:03 +0530

    Time: 163 ms (5.01%) | Memory: 23.9 MB (31.27%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -16,7 +16,9 @@ public:
                 if(intervals[j][0]<=end){
                     end= max(end,intervals[j][1]);
                 }
-                
+                else{
+                    break;
+                }
             }
             ans.push_back({start, end});
         }
